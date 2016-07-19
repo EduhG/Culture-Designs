4dcefba83fd49ae6b41179252147b3272032b23d
EduhG <edwinnyangena@gmail.com>
2016-07-20 00:28:28 +0300

    Added fixed header on scrolling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+jQuery(document).ready(function ($) {
+    var getHeaderHeight = $('.headerContainer').outerHeight();
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > getHeaderHeight) {
+            $('body').addClass('activeScroll').css('padding-top', getHeaderHeight);
+            $('headerContainer').css('top', 0);
+        } else {
+            $('body').removeClass('activeScroll').css('padding-top', 0);
+            $('headerContainer').css('top', '-' + getHeaderHeight + 'px');
+        }
+    });
+});
